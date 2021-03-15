d5880e8334487037614ab8bbfe386d621ad98366
Paul Cristian Martin <paul931224@gmail.com>
2021-03-15 18:27:58 +0100

    just messing around.

--- src/thomasa/morpheus/main.clj
+++ src/thomasa/morpheus/main.clj
@@ -6,26 +6,26 @@
 
 (defn usage [options-summary]
   (str/join
-   "\n"
-   ["Usage: morpheus -d DIR -f FORMAT paths"
-    options-summary]))
+    "\n"
+    ["Usage: morpheus -d DIR -f FORMAT paths"
+     options-summary]))
 
 (defn -main [& args]
   (let [{:keys [arguments errors summary] {:keys [dir format help]} :options}
         (cli/parse-opts
-         args
-         [["-d" "--dir DIR" "Directory to save output files to"
-           :parse-fn #(.getCanonicalFile (io/file %))
-           :validate [#(.exists %)]]
-          ["-f" "--format FORMAT" "dot, png, svg"
-           :default "dot"
-           :validate [#{"dot" "png" "svg"}]]
-          ["-h" "--help"]])]
+          args
+          [["-d" "--dir DIR" "Directory to save output files to"
+            :parse-fn #(.getCanonicalFile (io/file %))
+            :validate [#(.exists %)]]
+           ["-f" "--format FORMAT" "dot, png, svg"
+            :default "dot"
+            :validate [#{"dot" "png" "svg"}]]
+           ["-h" "--help"]])]
     (cond
       errors
       (println
-       "Errors occured while parsing command:\n"
-       (str/join "\n" errors))
+        "Errors occured while parsing command:\n"
+        (str/join "\n" errors))
 
       help
       (println (usage summary))
@@ -33,10 +33,11 @@
       :default
       (let [analysis (m/lint-analysis arguments)
             graph    (m/var-deps-graph analysis)
+            paul     (println (str "Graf: " graph))
             nodes    (map
-                      (fn [{:keys [ns name]}] (str ns "/" name))
-                      (:var-definitions analysis))]
+                       (fn [{:keys [ns name]}] (str ns "/" name))
+                       (:var-definitions analysis))]
         (doseq [node nodes]
           (-> (m/node->subgraph graph node)
-              (m/add-ref-to-subgraphs nodes format)
-              (m/graph->file! dir node format)))))))
+            (m/add-ref-to-subgraphs nodes format)
+            (m/graph->file! dir node format)))))))
